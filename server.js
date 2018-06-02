bf85432b86c98cca6be9b81ff264f8047e97c0b7
Pavel Grinevich <grinevich.ps@gmail.com>
2018-06-02 13:14:42 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const errorhandler = require('errorhandler');
+
+const app = express();
+
+app.use(errorhandler());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000');
+});
