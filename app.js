9a7ad55b2c7407fea6f31616ece294d277ac1867
Pavel Grinevich <grinevich.ps@gmail.com>
2018-06-04 18:56:48 +0300

    feat: add categories layout

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const config = require('./config');
 
 const app = express();
 
+app.engine('ejs', require('ejs-locals'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(errorhandler());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(config.get('port'), () => {
   console.log(`server listening on port ${config.get('port')}`);
